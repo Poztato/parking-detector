c9475678794ba13e54179535f4cf0f03cd5f386d
Poztato <leonardsu123@gmail.com>
2026-05-10 14:35:58 +0800

    Update occupancy_data.js

--- occupancy_data.js
+++ occupancy_data.js
@@ -2,343 +2,343 @@ const occupancyData = {
   "timestamp": "2026-01-30T00:05:31.711172",
   "slots": {
     "slot_1": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_2": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_3": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_4": {
-      "overlap_percentage": 0.9
+      "confidence_percentage": 0.9
     },
     "slot_5": {
-      "overlap_percentage": 82.09
+      "confidence_percentage": 82.09
     },
     "slot_6": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_7": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_8": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_9": {
-      "overlap_percentage": 78.88
+      "confidence_percentage": 78.88
     },
     "slot_10": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_11": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_12": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_13": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_14": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_15": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_16": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_17": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_18": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_19": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_20": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_21": {
-      "overlap_percentage": 7.46
+      "confidence_percentage": 7.46
     },
     "slot_22": {
-      "overlap_percentage": 91.52
+      "confidence_percentage": 91.52
     },
     "slot_23": {
-      "overlap_percentage": 10.51
+      "confidence_percentage": 10.51
     },
     "slot_24": {
-      "overlap_percentage": 90.97
+      "confidence_percentage": 90.97
     },
     "slot_25": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_26": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_27": {
-      "overlap_percentage": 13.08
+      "confidence_percentage": 13.08
     },
     "slot_28": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_29": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_30": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_31": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_32": {
-      "overlap_percentage": 88.57
+      "confidence_percentage": 88.57
     },
     "slot_33": {
-      "overlap_percentage": 97.08
+      "confidence_percentage": 97.08
     },
     "slot_34": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_35": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_36": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_37": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_38": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_39": {
-      "overlap_percentage": 88.72
+      "confidence_percentage": 88.72
     },
     "slot_40": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_41": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_42": {
-      "overlap_percentage": 94.86
+      "confidence_percentage": 94.86
     },
     "slot_43": {
-      "overlap_percentage": 0.22
+      "confidence_percentage": 0.22
     },
     "slot_44": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_45": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_46": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_47": {
-      "overlap_percentage": 78.21
+      "confidence_percentage": 78.21
     },
     "slot_48": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_49": {
-      "overlap_percentage": 52.13
+      "confidence_percentage": 52.13
     },
     "slot_50": {
-      "overlap_percentage": 98.27
+      "confidence_percentage": 98.27
     },
     "slot_51": {
-      "overlap_percentage": 77.23
+      "confidence_percentage": 77.23
     },
     "slot_52": {
-      "overlap_percentage": 86.89
+      "confidence_percentage": 86.89
     },
     "slot_53": {
-      "overlap_percentage": 82.35
+      "confidence_percentage": 82.35
     },
     "slot_54": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_55": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_56": {
-      "overlap_percentage": 92.96
+      "confidence_percentage": 92.96
     },
     "slot_57": {
-      "overlap_percentage": 11.05
+      "confidence_percentage": 11.05
     },
     "slot_58": {
-      "overlap_percentage": 99.85
+      "confidence_percentage": 99.85
     },
     "slot_59": {
-      "overlap_percentage": 13.52
+      "confidence_percentage": 13.52
     },
     "slot_60": {
-      "overlap_percentage": 100.0
+      "confidence_percentage": 100.0
     },
     "slot_61": {
-      "overlap_percentage": 100.0
+      "confidence_percentage": 100.0
     },
     "slot_62": {
-      "overlap_percentage": 96.66
+      "confidence_percentage": 96.66
     },
     "slot_63": {
-      "overlap_percentage": 97.28
+      "confidence_percentage": 97.28
     },
     "slot_64": {
-      "overlap_percentage": 99.62
+      "confidence_percentage": 99.62
     },
     "slot_65": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_66": {
-      "overlap_percentage": 3.88
+      "confidence_percentage": 3.88
     },
     "slot_67": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_68": {
-      "overlap_percentage": 96.2
+      "confidence_percentage": 96.2
     },
     "slot_69": {
-      "overlap_percentage": 29.25
+      "confidence_percentage": 29.25
     },
     "slot_70": {
-      "overlap_percentage": 18.2
+      "confidence_percentage": 18.2
     },
     "slot_71": {
-      "overlap_percentage": 14.01
+      "confidence_percentage": 14.01
     },
     "slot_72": {
-      "overlap_percentage": 96.81
+      "confidence_percentage": 96.81
     },
     "slot_73": {
-      "overlap_percentage": 0.18
+      "confidence_percentage": 0.18
     },
     "slot_74": {
-      "overlap_percentage": 0.09
+      "confidence_percentage": 0.09
     },
     "slot_75": {
-      "overlap_percentage": 99.18
+      "confidence_percentage": 99.18
     },
     "slot_76": {
-      "overlap_percentage": 1.24
+      "confidence_percentage": 1.24
     },
     "slot_77": {
-      "overlap_percentage": 99.56
+      "confidence_percentage": 99.56
     },
     "slot_78": {
-      "overlap_percentage": 97.97
+      "confidence_percentage": 97.97
     },
     "slot_79": {
-      "overlap_percentage": 99.41
+      "confidence_percentage": 99.41
     },
     "slot_80": {
-      "overlap_percentage": 96.57
+      "confidence_percentage": 96.57
     },
     "slot_81": {
-      "overlap_percentage": 95.53
+      "confidence_percentage": 95.53
     },
     "slot_82": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_83": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_84": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_85": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_86": {
-      "overlap_percentage": 81.46
+      "confidence_percentage": 81.46
     },
     "slot_87": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_88": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_89": {
-      "overlap_percentage": 94.79
+      "confidence_percentage": 94.79
     },
     "slot_90": {
-      "overlap_percentage": 0.18
+      "confidence_percentage": 0.18
     },
     "slot_91": {
-      "overlap_percentage": 93.01
+      "confidence_percentage": 93.01
     },
     "slot_92": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_93": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_94": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_95": {
-      "overlap_percentage": 99.68
+      "confidence_percentage": 99.68
     },
     "slot_96": {
-      "overlap_percentage": 99.73
+      "confidence_percentage": 99.73
     },
     "slot_97": {
-      "overlap_percentage": 17.17
+      "confidence_percentage": 17.17
     },
     "slot_98": {
-      "overlap_percentage": 98.02
+      "confidence_percentage": 98.02
     },
     "slot_99": {
-      "overlap_percentage": 10.17
+      "confidence_percentage": 10.17
     },
     "slot_100": {
-      "overlap_percentage": 17.0
+      "confidence_percentage": 17.0
     },
     "slot_101": {
-      "overlap_percentage": 6.32
+      "confidence_percentage": 6.32
     },
     "slot_102": {
-      "overlap_percentage": 83.22
+      "confidence_percentage": 83.22
     },
     "slot_103": {
-      "overlap_percentage": 93.93
+      "confidence_percentage": 93.93
     },
     "slot_104": {
-      "overlap_percentage": 0.0
+      "confidence_percentage": 0.0
     },
     "slot_105": {
-      "overlap_percentage": 88.97
+      "confidence_percentage": 88.97
     },
     "slot_106": {
-      "overlap_percentage": 94.67
+      "confidence_percentage": 94.67
     },
     "slot_107": {
-      "overlap_percentage": 97.94
+      "confidence_percentage": 97.94
     },
     "slot_108": {
-      "overlap_percentage": 96.45
+      "confidence_percentage": 96.45
     },
     "slot_109": {
-      "overlap_percentage": 98.36
+      "confidence_percentage": 98.36
     },
     "slot_110": {
-      "overlap_percentage": 97.94
+      "confidence_percentage": 97.94
     },
     "slot_111": {
-      "overlap_percentage": 97.92
+      "confidence_percentage": 97.92
     },
     "slot_112": {
-      "overlap_percentage": 100.0
+      "confidence_percentage": 100.0
     },
     "slot_113": {
-      "overlap_percentage": 98.8
+      "confidence_percentage": 98.8
     }
   }
 };
